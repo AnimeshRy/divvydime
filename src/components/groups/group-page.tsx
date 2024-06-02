8ff21230b2e5a0913bd7528e761a3724dcccfbe2
AnimeshRy <ani16september@gmail.com>
2024-06-02 22:29:36 +0530

    group_page fix

---
+++ src/components/groups/group-page.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { PropsWithChildren } from 'react'
+import { AddGroupByUrlButton } from './add-group-by-url'
+import { Button } from '@nextui-org/react'
+
+const GroupsPage = ({
+  children,
+  reload,
+}: PropsWithChildren<{ reload: () => void }>) => {
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2">
+        <h1 className="font-bold text-2xl flex-1">
+          <Link href="/groups">My groups</Link>
+        </h1>
+        <div className="flex gap-2">
+          <AddGroupByUrlButton reload={reload} />
+          <Button>
+            <Link href="/groups/create">
+              {/* <Plus className="w-4 h-4 mr-2" /> */}
+              <>Create</>
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <div>{children}</div>
+    </>
+  )
+}
+export default GroupsPage
